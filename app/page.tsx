73b3a0a79eb25d2340f39a24af6da3009bc7677a
Andrews <andrews.rodrigues@keepbuilding.eu>
2025-11-07 16:17:37 -0300

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -83,6 +83,7 @@ useEffect(() => {
     const handler = () => loadProposals();
     contract.on("ProposalCreated", handler);
     return () => contract.removeListener("ProposalCreated", handler);
+     }, [contractAddress, hasMM]);
     
 
       return (
